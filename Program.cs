ff8e07da0050e71fcb44e97caf412daa7cbe2e99
Ai Uragaki <ai.uragaki.nz@gmail.com>
2021-06-19 22:41:38 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _8Characters
+{
+    class Program
+    {
+        static void Main(string[] args) 
+            //Declare and initialize array of 8 characters then print the character elements in reverse order. ( From last element to first element)
+        {
+            string[] Characters = new string[8] { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Holiday" };
+
+            for (int i = Characters.Length-1; i >=0; i--)
+            {
+                Console.WriteLine(Characters[i]);
+            }
+                
+
+            
+        }
+        
+
+    }
+}
